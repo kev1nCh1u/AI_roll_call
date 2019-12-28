46b207194720a3d1d903068635fb5e1505c8849f
KevinChiu <fsd83811@gmail.com>
2019-12-28 23:33:29 +0800

    start

---
+++ src/rollCall.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+
+dirname, _= os.path.split(os.path.abspath(__file__))    #取得檔案路徑
+path = r"" + dirname
+cl = ["四電四丙", "四電四丙", "四電四丙", "四電四丙"]  
+name = ["邱暐凱","吳定軒","劉建廷","林舒潔"]
+num = ["1105164318","1105105340","1105448787","6644666666"]
+att = ["X", "X", "X", "X"]
+student_list = {"班級": cl,  
+        "學生": name,
+        "學號":num,
+        "出缺席":att
+        }
+select_df = pd.DataFrame(student_list)
+select_df.to_csv(r"" + path + "\\student.csv",index=False,encoding='utf_8_sig')
+#select_df.to_csv(r"" + path + "\\student.csv",index=False,sep=',',encoding='utf_8_sig')
+
+print("---student_list")
+print(select_df) # 回傳資料內容
+
+def checkStudent(student):    
+    for i in range(0, len(name)):
+        if  student == name[i]:
+            print(i)
+    out_df = select_df[select_df.loc[:,"學生"] == student].index
+    select_df.iloc[out_df.values,3] = "V" 
+    out_df1 = select_df[select_df.loc[:,"學生"] == student] #印出確認
+    print(out_df1)
+    select_df.to_csv(r"" + path + "\\student.csv",index=False,encoding='utf_8_sig')
+
+for i in ["邱暐凱","吳定軒","劉建廷","林舒潔"]:
+    checkStudent(i)
